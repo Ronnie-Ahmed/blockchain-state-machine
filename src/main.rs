efa743aba12d5c6ed3081e5df1b7a510e465bdd6
Ronnie-Ahmed <rksraisul@gmail.com>
2024-04-16 22:35:00 +0600

    runtime

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 mod balances;
 mod system;
+
+
+pub struct Runtime{
+	system:system::Pallet,
+	balance:balances::Pallet
+}
+
+
+impl Runtime{
+	fn new()->Self{
+		Self { system: system::Pallet::new(), balance: balances::Pallet::new() }
+
+	}
+} 
+
+
+
 fn main() {
 	println!("Hello, world!");
 }

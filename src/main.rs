afc6163f384ea9bcc866f5288a5fab39d8af5696
Ronnie-Ahmed <rksraisul@gmail.com>
2024-04-19 00:32:33 +0600

    RuntimeCall

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,14 @@ mod types{
 	pub type Balance=u128;
 	pub type BlockNumber=u32;
 	pub type Nonce=u32;
+	pub type Extrinsic=crate::support::Extrinsic<AccountId,crate::RuntimeCall>;
+	pub type Header=crate::support::Header<BlockNumber>;
+	pub type Block=crate::support::Block<Header,Extrinsic>;
+
+}
+
+pub enum RuntimeCall{
+
 }
 
 
